b7eff8241277d0a024cce5301bf4cde2e8b7cc99
Shashank Rajput <rajput.shashank11@gmail.com>
2021-08-16 17:35:09 +0000

    Added custom sampler for single shuffle - RandomSamplerSS

--- main.py
+++ main.py
@@ -14,11 +14,15 @@ import argparse
 from models import *
 from utils import progress_bar
 
+from torch.utils.data import RandomSampler
+import sys
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
 parser.add_argument('--resume', '-r', action='store_true',
                     help='resume from checkpoint')
+
+parser.add_argument('--sampling', default=None, type=str, help='sampling type, one of RR, SS or SGD')
 args = parser.parse_args()
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -39,10 +43,40 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
+class RandomSamplerSS(RandomSampler):
+    def __init__(self, train_dataset):
+        super().__init__(train_dataset)
+        self.epoch=1
+        self.permutation=None
+    def __iter__(self):
+        n = len(self.data_source)
+        if self.epoch==1:
+            generator = torch.Generator()
+            generator.manual_seed(int(torch.empty((), dtype=torch.int64).random_().item()))
+            self.permutation = torch.randperm(n, generator=generator).tolist()
+            yield from self.permutation
+        else:
+            # self.permutation.reverse()
+            yield from self.permutation
+        self.epoch=self.epoch+1
+
+
 trainset = torchvision.datasets.CIFAR10(
     root='./data', train=True, download=True, transform=transform_train)
+
+custom_sampler = None
+sampling = args.sampling
+if sampling=="SGD":
+    custom_sampler = RandomSampler(data_source=trainset, replacement=True)
+elif sampling=="SS":
+    custom_sampler = RandomSamplerSS(trainset)
+elif sampling=="RR":
+    custom_sampler = RandomSampler(data_source=trainset)
+else:
+    raise ValueError("--sampling argument should be one of SGD, SS or RR.")
+
 trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=128, shuffle=True, num_workers=2)
+    trainset, batch_size=128, sampler=custom_sampler, num_workers=2)
 
 testset = torchvision.datasets.CIFAR10(
     root='./data', train=False, download=True, transform=transform_test)
@@ -54,7 +88,8 @@ classes = ('plane', 'car', 'bird', 'cat', 'deer',
 
 # Model
 print('==> Building model..')
-# net = VGG('VGG19')
+model_name='VGG11'
+net = VGG('VGG11')
 # net = ResNet18()
 # net = PreActResNet18()
 # net = GoogLeNet()
@@ -68,7 +103,7 @@ print('==> Building model..')
 # net = ShuffleNetV2(1)
 # net = EfficientNetB0()
 # net = RegNetX_200MF()
-net = SimpleDLA()
+# net = SimpleDLA()
 net = net.to(device)
 if device == 'cuda':
     net = torch.nn.DataParallel(net)
@@ -93,9 +128,9 @@ scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
 def train(epoch):
     print('\nEpoch: %d' % epoch)
     net.train()
-    train_loss = 0
-    correct = 0
-    total = 0
+    # train_loss = 0
+    # correct = 0
+    # total = 0
     for batch_idx, (inputs, targets) in enumerate(trainloader):
         inputs, targets = inputs.to(device), targets.to(device)
         optimizer.zero_grad()
@@ -104,13 +139,29 @@ def train(epoch):
         loss.backward()
         optimizer.step()
 
-        train_loss += loss.item()
-        _, predicted = outputs.max(1)
-        total += targets.size(0)
-        correct += predicted.eq(targets).sum().item()
+        # train_loss += loss.item()
+        # _, predicted = outputs.max(1)
+        # total += targets.size(0)
+        # correct += predicted.eq(targets).sum().item()
 
-        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                     % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+        # progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+        #              % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+    
+
+    train_loss = 0    
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(trainloader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = net(inputs)
+            loss = criterion(outputs, targets)
+
+            train_loss += loss.item()
+    print(train_loss)
+    f = open("./results/"+model_name+"_"+sampling, "a")
+    f.write(str(epoch)+" "+str(train_loss))
+    f.write("\n")
+    f.close()
+
 
 
 def test(epoch):
@@ -148,7 +199,8 @@ def test(epoch):
         best_acc = acc
 
 
+
 for epoch in range(start_epoch, start_epoch+200):
     train(epoch)
-    test(epoch)
+    # test(epoch)
     scheduler.step()
