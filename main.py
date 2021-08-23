c744eb9a9370134b8f5a7eaac56bb7f25b629539
Shashank Rajput <rajput.shashank11@gmail.com>
2021-08-23 23:15:30 +0000

    Added code to do multiple runs of training

--- main.py
+++ main.py
@@ -20,29 +20,8 @@ import time
 import random
 import pickle
 
-parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
-parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
 
-parser.add_argument('--sampling', default=None, type=str, help='sampling type, one of RR, SS, or SGD')
-parser.add_argument('--model', default=None, type=str, help='model, one of VGG11, VGG13, VGG16, or VGG19')
-parser.add_argument('--batchnorm', default=None, type=str, help='batchnorm, either true or false')
-args = parser.parse_args()
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-# Data
-print('==> Preparing data..')
-transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
-
-transform_test = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
 
 class RandomSamplerSS(RandomSampler):
     def __init__(self, train_dataset):
@@ -135,93 +114,8 @@ class RandomSamplerSGD_classmix(RandomSampler):
         self.epoch=self.epoch+1
 
 
-trainset = torchvision.datasets.CIFAR10(
-    root='./data', train=True, download=True, transform=transform_train)
-
-custom_sampler = None
-sampling = args.sampling
-if sampling=="SGD":
-    custom_sampler = RandomSampler(data_source=trainset, replacement=True)
-elif sampling=="SS":
-    custom_sampler = RandomSamplerSS(trainset)
-elif sampling=="SS_mix":
-    custom_sampler = RandomSamplerSS_classmix(trainset)
-elif sampling=="RR_mix":
-    custom_sampler = RandomSamplerRR_classmix(trainset)
-elif sampling=="SGD_mix":
-    custom_sampler = RandomSamplerSGD_classmix(trainset)
-elif sampling=="RR":
-    custom_sampler = RandomSampler(data_source=trainset)
-else:
-    raise ValueError("--sampling argument should be one of SGD, SS, or RR.")
-
-trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=128, sampler=custom_sampler, num_workers=2)
-
-trainloader_eval = torch.utils.data.DataLoader(
-    trainset, batch_size=128, shuffle=False, num_workers=2)
-
-testset = torchvision.datasets.CIFAR10(
-    root='./data', train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(
-    testset, batch_size=100, shuffle=False, num_workers=2)
-
-classes = ('plane', 'car', 'bird', 'cat', 'deer',
-           'dog', 'frog', 'horse', 'ship', 'truck')
-
-# Model
-
-model = args.model
-if model not in ['VGG11', 'VGG13', 'VGG16', 'VGG19']:
-    raise ValueError("--model argument should be one of VGG11, VGG13, VGG16 or VGG19.")
-
-batchnorm = args.batchnorm
-if batchnorm not in ['true', 'false']:
-    raise ValueError("--batchnorm should be either true or false.")
-
-batchnorm = (batchnorm=='true')
-btnm='batchnorm_false'
-if batchnorm:
-    btnm='batchnorm_true'
-
-print('==> Building model..')
-
-net = VGG(model, batchnorm)
-
-# model="ResNet101"
-# net = ResNet101()
-
-# net = PreActResNet18()
-# net = GoogLeNet()
-# net = DenseNet121()
-# net = ResNeXt29_2x64d()
-# net = MobileNet()
-# net = MobileNetV2()
-# net = DPN92()
-# net = ShuffleNetG2()
-# net = SENet18()
-# net = ShuffleNetV2(1)
-# net = EfficientNetB0()
-# net = RegNetX_200MF()
-
-# model="SimpleDLA"
-# net = SimpleDLA()
-
-net = net.to(device)
-if device == 'cuda':
-    net = torch.nn.DataParallel(net)
-    cudnn.benchmark = True
-
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=args.lr,
-                      momentum=0.9, weight_decay=5e-4)
-scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
-
-
-filename="./results/"+model+"_"+sampling+"_"+btnm
-print("Output will be written to "+filename)
 # Training
-def train():
+def train(net, trainloader, device, criterion, optimizer):
     start_time=time.time()
     net.train()
     for batch_idx, (inputs, targets) in enumerate(trainloader):
@@ -235,7 +129,7 @@ def train():
 
 
 # Evaluation
-def evaluate():
+def evaluate(net, trainloader_eval, testloader, device, criterion, optimizer):
     net.eval()
 
     train_loss = 0    
@@ -270,27 +164,153 @@ def evaluate():
     return evaluation
 
 
-num_epochs=200
-results=[]
+
+
+
+
+parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+
+parser.add_argument('--sampling', default=None, type=str, help='sampling type, one of RR, SS, or SGD')
+parser.add_argument('--model', default=None, type=str, help='model, one of VGG11, VGG13, VGG16, or VGG19')
+parser.add_argument('--batchnorm', default=None, type=str, help='batchnorm, either true or false')
+args = parser.parse_args()
+
+
+model = args.model
+if model not in ['VGG11', 'VGG13', 'VGG16', 'VGG19']:
+    raise ValueError("--model argument should be one of VGG11, VGG13, VGG16, or VGG19.")
+
+sampling = args.sampling
+if sampling not in ['RR', 'SS', 'SGD', 'RR_mix', 'SS_mix', 'SGD_mix']:
+    raise ValueError("--sampling argument should be one of RR, SS, SGD, RR_mix, SS_mix, or SGD_mix.")
+
+batchnorm = args.batchnorm
+if batchnorm not in ['true', 'false']:
+    raise ValueError("--batchnorm should be either true or false.")
+
+batchnorm = (batchnorm=='true')
+btnm='batchnorm_false'
+if batchnorm:
+    btnm='batchnorm_true'
+
+
+filename="./results/"+model+"_"+sampling+"_"+btnm
+print("Output will be written to "+filename)
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+num_runs=3
+num_epochs=10
+results={}
 with open(filename, 'w') as f:
-    for epoch in range(0, num_epochs):
-        print('\nEpoch: %d' % epoch)
-        
-        train_time=train()
-        evaluation=evaluate()
+    for run in range(num_runs):
+        # Data
+        print('==> Preparing data..')
+        transform_train = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
 
-        epoch_result={}
-        epoch_result["Epoch"]=epoch
-        epoch_result["Training time"]=train_time
-        epoch_result.update(evaluation)
-        results.append(epoch_result)
-        
-        print(epoch_result)
-        f = open(filename, "a")
-        f.write(str(epoch_result)+os.linesep)
-        f.flush()
+        transform_test = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
 
-        scheduler.step()
+
+
+
+        trainset = torchvision.datasets.CIFAR10(
+            root='./data', train=True, download=True, transform=transform_train)
+
+        custom_sampler = None
+        if sampling=="SGD":
+            custom_sampler = RandomSampler(data_source=trainset, replacement=True)
+        elif sampling=="SS":
+            custom_sampler = RandomSamplerSS(trainset)
+        elif sampling=="SS_mix":
+            custom_sampler = RandomSamplerSS_classmix(trainset)
+        elif sampling=="RR_mix":
+            custom_sampler = RandomSamplerRR_classmix(trainset)
+        elif sampling=="SGD_mix":
+            custom_sampler = RandomSamplerSGD_classmix(trainset)
+        elif sampling=="RR":
+            custom_sampler = RandomSampler(data_source=trainset)
+        else:
+            raise ValueError("--sampling argument should be one of SGD, SS, or RR.")
+
+        trainloader = torch.utils.data.DataLoader(
+            trainset, batch_size=128, sampler=custom_sampler, num_workers=2)
+
+        trainloader_eval = torch.utils.data.DataLoader(
+            trainset, batch_size=128, shuffle=False, num_workers=2)
+
+        testset = torchvision.datasets.CIFAR10(
+            root='./data', train=False, download=True, transform=transform_test)
+        testloader = torch.utils.data.DataLoader(
+            testset, batch_size=100, shuffle=False, num_workers=2)
+
+
+
+
+        print('==> Building model..')
+
+        net = VGG(model, batchnorm)
+
+        # model="ResNet101"
+        # net = ResNet101()
+
+        # net = PreActResNet18()
+        # net = GoogLeNet()
+        # net = DenseNet121()
+        # net = ResNeXt29_2x64d()
+        # net = MobileNet()
+        # net = MobileNetV2()
+        # net = DPN92()
+        # net = ShuffleNetG2()
+        # net = SENet18()
+        # net = ShuffleNetV2(1)
+        # net = EfficientNetB0()
+        # net = RegNetX_200MF()
+
+        # model="SimpleDLA"
+        # net = SimpleDLA()
+
+        net = net.to(device)
+        if device == 'cuda':
+            net = torch.nn.DataParallel(net)
+            cudnn.benchmark = True
+
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.SGD(net.parameters(), lr=args.lr,
+                            momentum=0.9, weight_decay=5e-4)
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
+
+
+    
+        results[run]={}
+        for epoch in range(0, num_epochs):
+                        
+            train_time=train(net, trainloader, device, criterion, optimizer)
+            evaluation=evaluate(net, trainloader_eval, testloader, device, criterion, optimizer)
+
+            epoch_result={}
+            epoch_result["Run"]=run
+            epoch_result["Epoch"]=epoch
+            epoch_result["Training Time"]=train_time
+            epoch_result.update(evaluation)
+        
+            results[run][epoch]=epoch_result            
+            print(epoch_result)
+
+            f = open(filename, "a")
+            f.write(str(epoch_result)+os.linesep)
+            f.flush()
+
+            scheduler.step()
 
 with open(filename+".pkl",'wb') as f:
     pickle.dump(results,f)
